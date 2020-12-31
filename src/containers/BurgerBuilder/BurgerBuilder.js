7a2889a073cf57613f411179956a9fa3ec669bc9
Nick Brubaker <nbrubaker@axio.com>
2020-12-31 14:42:58 -0500

    initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class BurgerBuilder extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </React.Fragment>
+    );
+  }
+}
